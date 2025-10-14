3e3b3d9835a652e6fc900700e6f26b11953274c9
Kifrx <zik.min02@gmail.com>
2025-10-14 12:19:15 +0700

    Update Vendingcoffe.java

--- Vendingcoffe.java
+++ Vendingcoffe.java
@@ -1,34 +1,41 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class VendingCoffe {
-    private Map<String, Integer> menuCoffe;
+    private ArrayList<String> namaMenu;
+    private ArrayList<Integer> hargaMenu;
     private int totalPrice;
 
     public VendingCoffe() {
-        menuCoffe = new HashMap<>();
+        namaMenu = new ArrayList<>();
+        hargaMenu = new ArrayList<>();
         totalPrice = 0;
 
-        menuCoffe.put("Espresso", 10000);
-        menuCoffe.put("Americano", 12000);
-        menuCoffe.put("Latte", 15000);
-        menuCoffe.put("Cappuccino", 15000);
+        // menu
+        tambahMenu("Espresso", 10000);
+        tambahMenu("Americano", 12000);
+        tambahMenu("Latte", 15000);
+        tambahMenu("Cappuccino", 15000);
+    }
+
+    private void tambahMenu(String nama, int harga) {
+        namaMenu.add(nama);
+        hargaMenu.add(harga);
     }
 
     public void tampilkanMenu() {
-        System.out.println("===== MENU KOPI =====");
-        for (Map.Entry<String, Integer> entry : menuCoffe.entrySet()) {
-            System.out.println("- " + entry.getKey() + " : Rp" + entry.getValue());
+        System.out.println("=== MENU KOPI ===");
+        for (int i = 0; i < namaMenu.size(); i++) {
+            System.out.println((i + 1) + ". " + namaMenu.get(i) + " - Rp" + hargaMenu.get(i));
         }
     }
 
-    public int pilihMenu(String namaKopi) {
-        if (menuCoffe.containsKey(namaKopi)) {
-            totalPrice = menuCoffe.get(namaKopi);
+    public int pilihMenu(int nomor) {
+        if (nomor >= 1 && nomor <= namaMenu.size()) {
+            totalPrice = hargaMenu.get(nomor - 1);
+            System.out.println("Anda memilih: " + namaMenu.get(nomor - 1));
             return totalPrice;
         } else {
-            System.out.println("Menu tidak tersedia!");
+            System.out.println("Nomor menu tidak valid.");
             return 0;
         }
     }
@@ -39,12 +46,19 @@ public class VendingCoffe {
             System.out.println(" Pembayaran berhasil! Kembalian: Rp" + kembalian);
             return true;
         } else {
-            System.out.println(" Uang tidak mencukupi");
+            System.out.println("Uang tidak cukup. Transaksi dibatalkan.");
             return false;
         }
     }
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    public String getNamaMenu(int nomor) {
+        if (nomor >= 1 && nomor <= namaMenu.size()) {
+            return namaMenu.get(nomor - 1);
+        }
+        return "Tidak diketahui";
     }
 }
